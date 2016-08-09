4852cef5e892909f194c14cc05f0f73988a9a7f1
= <=>
2016-08-09 14:20:25 +0800

    delete

--- src/actions/index.js
+++
@@ -1,6 +0,0 @@
-export const changeIframe = (href) => {
-	return {
-		type: 'CHANGE_IFRAME',
-		href
-	}
-}
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import iframeHref from './iframeHref';
-
-const CSS3App = combineReducers({
-	iframeHref,
-})
-
-export default CSS3App
